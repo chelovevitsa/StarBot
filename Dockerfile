c862e07b61b95516e893d5e13a2829896251c92f
chelovevitsa <vita.bosatckaia30@gmail.com>
2025-06-09 19:15:21 +0200

    add token

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,13 @@
 FROM python:3.11-slim
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+COPY start.sh .
+RUN chmod +x start.sh
+
+CMD ["./start.sh"]
--- starbot.py
+++ starbot.py
@@ -1,8 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8183658865:AAHQjtIJWA8d_yk7cPceKFZ2f8x1riijxH0"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bla bla!")
